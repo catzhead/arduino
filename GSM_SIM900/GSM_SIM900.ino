2ef8aad255f5d54e6120b2c27e6f5043059bcfa4
catzhead <catzhead@hotmail.com>
2017-12-14 22:08:38 +0100

    ca marche avec SIM900

---
+++ GSM_SIM900/GSM_SIM900.ino
@@ -0,0 +1,44 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial SIM900(7, 8);
+
+void setup() {
+  // Arduino communicates with SIM900 GSM shield at a baud rate of 19200
+  // Make sure that corresponds to the baud rate of your module
+  SIM900.begin(19200);
+  // Give time to your GSM shield log on to network
+  delay(20000);   
+  
+  // Send the SMS
+  sendSMS();
+}
+
+void loop() {
+}
+
+void sendSMS() {
+  // put your main code here, to run repeatedly:
+  // AT command to set SIM900 to SMS mode
+  SIM900.print("AT+CMGF=1\r"); 
+  delay(100);
+
+  SIM900.println("AT+CPIN=\"1234\"");
+  delay(100);
+
+  // REPLACE THE X's WITH THE RECIPIENT'S MOBILE NUMBER
+  // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
+  SIM900.println("AT + CMGS = \"0781428861\""); 
+  delay(100);
+  
+  // REPLACE WITH YOUR OWN SMS MESSAGE CONTENT
+  SIM900.println("Message example from Arduino Uno."); 
+  delay(100);
+
+  // End AT command with a ^Z, ASCII code 26
+  SIM900.println((char)26); 
+  delay(100);
+  SIM900.println();
+  // Give module time to send SMS
+  delay(5000); 
+}
