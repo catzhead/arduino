72205447edf67c60c92157bd58e374f89acd9dac
Adrien Barbot <catzhead@hotmail.com>
2019-12-16 15:25:04 +0100

    directory cleanup

--- moulin/buttons.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BUTTONS__
-#define __BUTTONS__
-
-int get_button();
-
-#endif
--- moulin/gsm.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GSM__
-#define __GSM__
-
-void GSM_init();
-void GSM_start();
-void GSM_send_SMS(const char *);
-void GSM_stop();
-
-#endif
